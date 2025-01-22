b5821197efc655a99460a895283353ede78aee60
ANDREA BIODDO s332145 <s332145@studenti.polito.it>
2025-01-22 20:21:34 +0100

    added project

---
+++ s332145.py
@@ -0,0 +1,26 @@
+import numpy as np
+
+
+def f1(x: np.ndarray) -> np.ndarray:
+    return np.sin(x[0])
+
+def f2(x: np.ndarray) -> np.ndarray:
+    return np.subtract(np.subtract(np.multiply(np.multiply(np.true_divide(np.multiply(x[0], -12.010071451490873), 0.937155475657861), np.multiply(np.add(x[2], 42.82436112375842), -2005.4050314653837)), 0.991200953825132), np.multiply(np.add(-4.992884595538726, np.negative(np.true_divide(x[1], x[2]))), -0.8369987928839783)), np.negative(np.negative(np.multiply(-77.31860957983632, x[0]))))
+
+def f3(x: np.ndarray) -> np.ndarray:
+    return np.add(np.add(np.subtract(4.34535355, np.multiply(x[2], 3.5000012111111)), np.multiply(x[0], np.add(x[0], x[0]))), np.multiply(np.multiply(x[1], x[1]), np.subtract(0, x[1])))
+
+def f4(x: np.ndarray) -> np.ndarray:
+    return np.add(np.sin(np.log10(np.true_divide(np.multiply(0.6071, np.subtract(86.099, x[1])), 0.9788403892008104))), np.multiply(np.cos(x[1]), 6.257581537096485))
+
+def f5(x: np.ndarray) -> np.ndarray:
+    return np.true_divide(x[0], np.add(np.subtract(74.23268365923897, np.true_divide(np.exp(np.negative(-23.09467604461453)), x[1])), x[1]))
+
+def f6(x: np.ndarray) -> np.ndarray:
+    return np.subtract(np.multiply(46.729954092586155, np.true_divide(x[1], 29.693475084449858)), np.add(np.true_divide(np.multiply(-19.12195753421895, x[0]), 71.95484872276394), np.add(x[0], np.true_divide(0.11095603722279583, np.add(np.multiply(61.91286208690539, x[1]), np.true_divide(x[1], 52.480913742761885))))))
+
+def f7(x: np.ndarray) -> np.ndarray:
+    return np.abs(np.true_divide(np.multiply(126.8257610830577, np.multiply(np.true_divide(x[0], 24), x[1])), np.add(np.abs(np.subtract(x[0], x[1])), 0.026551154023966794)))
+
+def f8(x: np.ndarray) -> np.ndarray:
+    return np.multiply(97.45457647685816, np.multiply(x[5], np.multiply(x[5], x[5])))
