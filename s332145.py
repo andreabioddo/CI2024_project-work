ee007fc086a7e8176b0f81de3ec724d40bd401a8
ANDREA BIODDO s332145 <s332145@studenti.polito.it>
2025-02-11 17:18:16 +0100

    updated

--- s332145.py
+++ s332145.py
@@ -5,13 +5,14 @@ def f1(x: np.ndarray) -> np.ndarray:
     return np.sin(x[0])
 
 def f2(x: np.ndarray) -> np.ndarray:
-    return np.subtract(np.subtract(np.multiply(np.multiply(np.true_divide(np.multiply(x[0], -12.010071451490873), 0.937155475657861), np.multiply(np.add(x[2], 42.82436112375842), -2005.4050314653837)), 0.991200953825132), np.multiply(np.add(-4.992884595538726, np.negative(np.true_divide(x[1], x[2]))), -0.8369987928839783)), np.negative(np.negative(np.multiply(-77.31860957983632, x[0]))))
+    return np.multiply(np.add(np.multiply(np.add(np.add(x[1], x[1]), x[0]), np.cos(np.sqrt(np.abs(np.add(x[2], x[0]))))), np.add(np.multiply(np.add(x[1], x[2]), np.divide(np.sin(x[0]), np.add(1e-20, x[0]))), np.multiply(2, np.add(x[0], np.sin(x[0]))))), np.multiply(233333,1182121210, 2.9888271728232))
 
-def f3(x: np.ndarray) -> np.ndarray:
-    return np.add(np.add(np.subtract(4.34535355, np.multiply(x[2], 3.5000012111111)), np.multiply(x[0], np.add(x[0], x[0]))), np.multiply(np.multiply(x[1], x[1]), np.subtract(0, x[1])))
+
+def f3(x: np.ndarray) -> np.ndarray: return np.multiply(np.add(np.add(np.multiply(np.subtract(0, x[1]), np.multiply(np.add(x[1], 0), np.add(0, x[1]))), np.multiply(np.add(np.add(x[0], 0), np.add(x[0], 0)), x[0])), np.subtract(np.add(4, 0), np.multiply(np.divide(np.add(7, 0), np.add(2, 0)), x[2]))), np.add(0, 1))
+
 
 def f4(x: np.ndarray) -> np.ndarray:
-    return np.add(np.sin(np.log10(np.true_divide(np.multiply(0.6071, np.subtract(86.099, x[1])), 0.9788403892008104))), np.multiply(np.cos(x[1]), 6.257581537096485))
+    return np.add(np.add(np.multiply(7.009101021211, np.cos(x[1])), np.log(np.add(15.01001232332, 11.9888271728))), np.divide(np.multiply(7, x[0]), np.multiply(-15.01001232332, 4.9888271728)))
 
 def f5(x: np.ndarray) -> np.ndarray:
     return np.true_divide(x[0], np.add(np.subtract(74.23268365923897, np.true_divide(np.exp(np.negative(-23.09467604461453)), x[1])), x[1]))
